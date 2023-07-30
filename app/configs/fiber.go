7ddb4e198f325461d5f5a98c79482efc11d71ccb
firhan abigael <firhangael1525@gmail.com>
2023-07-31 04:39:06 +0700

    add custome error handler

--- app/configs/fiber.go
+++ app/configs/fiber.go
@@ -1,6 +1,8 @@
 package configs
 
 import (
+	"github.com/Firhan384/restfull-api-golang/app/schemas"
+	"github.com/Firhan384/restfull-api-golang/app/utils"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -8,5 +10,17 @@ func NewConfigFiber(cfg ConfigApp) fiber.Config {
 	return fiber.Config{
 		AppName:               cfg.AppName,
 		DisableStartupMessage: true,
+		ErrorHandler: func(ctx *fiber.Ctx, err error) error {
+			resp := schemas.NewResponseApi()
+
+			if err == fiber.ErrInternalServerError {
+				resp.StatusCode = fiber.StatusInternalServerError
+				resp.Message = utils.StatusText(fiber.StatusInternalServerError)
+				resp.Error = true
+
+				return ctx.Status(fiber.StatusInternalServerError).JSON(resp)
+			}
+			return nil
+		},
 	}
 }
