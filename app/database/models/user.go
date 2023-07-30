a426de81259bf6130d3fdfbc0e3354672ab4af53
firhan abigael <firhangael1525@gmail.com>
2023-07-31 02:54:02 +0700

    add hook BeforeCreate

--- app/database/models/user.go
+++ app/database/models/user.go
@@ -1,10 +1,23 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type User struct {
-	gorm.Model
-	Fullname string `gorm:"not null"`
-	Email    string `gorm:"unique"`
-	Password string `gorm:"not null"`
+	ID        string `gorm:"primarykey"`
+	Fullname  string `gorm:"not null"`
+	Email     string `gorm:"unique"`
+	Password  string `gorm:"not null"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
+
+func (u *User) BeforeCreate(tx *gorm.DB) (err error) {
+	u.ID = uuid.New().String()
+	return
 }
