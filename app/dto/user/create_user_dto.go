62e457357ed9634bc2298805e5997ae96718730e
firhan abigael <firhangael1525@gmail.com>
2023-07-31 02:41:29 +0700

    fix typo validator in dto

--- app/dto/user/create_user_dto.go
+++ app/dto/user/create_user_dto.go
@@ -1,7 +1,7 @@
 package dto
 
 type CreateUserDTO struct {
-	Fullname string `json:"fullname" validate:"required,min|2"`
+	Fullname string `json:"fullname" validate:"required,min=2"`
 	Email    string `json:"email" validate:"required,email"`
 	Password string `json:"password" validate:"required"`
 }
